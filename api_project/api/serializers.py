5a32d695ab68acb915a0ed7661c40a0f799a4a67
Scarlet Scherzinger <scarletscherzinger@gmail.com>
2025-11-23 00:19:18 +0300

    Add BookSerializer, BookList API view, and URL routing for books endpoint

---
+++ api_project/api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Book
+
+class BookSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Book model.
+    Converts Book model instances to JSON format and vice versa.
+    """
+    class Meta:
+        model = Book
+        fields = '__all__'  # Include all fields: id, title, author
