2b800753a1abbbe2693346a16742e010aca18dbc
Scarlet Scherzinger <scarletscherzinger@gmail.com>
2025-12-05 16:49:51 +0300

    Configure PostgreSQL database for Django blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_blog_db',
+        'USER': 'django_user',
+        'PASSWORD': 'django_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
