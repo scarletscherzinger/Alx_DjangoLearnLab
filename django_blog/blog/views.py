0e5024b2038c1b57dcb0b41a4ca1841c0f8a4a61
Scarlet Scherzinger <scarletscherzinger@gmail.com>
2025-12-07 01:50:31 +0300

    Fix URL pattern to use pk instead of post_id for comment creation

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -108,16 +108,16 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
     template_name = 'blog/add_comment.html'
     
     def form_valid(self, form):
-        form.instance.post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        form.instance.post = get_object_or_404(Post, pk=self.kwargs['pk'])
         form.instance.author = self.request.user
         return super().form_valid(form)
     
     def get_success_url(self):
-        return reverse_lazy('post-detail', kwargs={'pk': self.kwargs['post_id']})
+        return reverse_lazy('post-detail', kwargs={'pk': self.kwargs['pk']})
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['post'] = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        context['post'] = get_object_or_404(Post, pk=self.kwargs['pk'])
         return context
 
 # Update Comment
