83a49cb477e99358aa713b2a056e356abd07f3c3
Scarlet Scherzinger <scarletscherzinger@gmail.com>
2025-12-13 08:11:50 +0300

    Fix serializer to use get_user_model().objects.create_user explicitly

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -12,16 +12,16 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
             password=validated_data['password'],
-            bio=validated_data.get('bio', ''),
-            profile_picture=validated_data.get('profile_picture', None)
         )
+        user.bio = validated_data.get('bio', '')
+        user.profile_picture = validated_data.get('profile_picture', None)
+        user.save()
         Token.objects.create(user=user)
         return user
-
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
